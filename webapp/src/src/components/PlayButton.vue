6fb5ea2419a3d98cbc6c2471f85c9be35ba00870
Pierre RABY <pierre.raby.pro@gmail.com>
2017-09-20 16:13:02 +0200

    Fix f[oweuhf[ieqwuhpqweiub]]

--- webapp/src/src/components/PlayButton.vue
+++ webapp/src/src/components/PlayButton.vue
@@ -10,24 +10,23 @@
 
 // eslint-disable-next-line new-cap
   let sound = new buzz.sound(require('../assets/sound.mp3'))
+  let timerValue = '00:00'
 
   export default {
     name: 'play-button',
     data () {
       return {
         msg: 'Play',
-        timer: '00:00'
+        timer: timerValue
       }
     },
     methods: {
       startNewExtract () {
         sound.play()
         sound.bind('timeupdate', function () {
-          this.timer = buzz.toTimer(this.getTime())
-          console.log(this.timer)
+          timerValue = buzz.toTimer(this.getTime())
+          console.log(timerValue)
         })
-// eslint-disable-next-line spaced-comment
-        //TODO: Load next 2 songs from backend. EDIT : DEGAGE CA AU DESSUS FDP !
       }
     }
   }
