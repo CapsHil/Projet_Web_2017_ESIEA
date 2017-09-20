bb7479848b47a2b1249a67e72c04d2ef4c8b6e63
Pierre RABY <pierre.raby.pro@gmail.com>
2017-09-20 14:11:25 +0200

    DTG Gilles

--- webapp/src/src/components/PlayButton.vue
+++ webapp/src/src/components/PlayButton.vue
@@ -1,21 +1,26 @@
 <template>
   <div class="play-button">
-    <button v-on:click="">{{msg}}</button>
+    <button v-on:click="playSound()">{{msg}}</button>
   </div>
 </template>
 
 <script>
   import buzz from 'buzz'
+
+// eslint-disable-next-line new-cap
+  let sound = new buzz.sound(require('../assets/sound.mp3'))
+
   export default {
     name: 'play-button',
     data () {
       return {
-        msg: 'Play',
-        sound: buzz.sound('/sound.mp3')
+        msg: 'Play'
       }
     },
     methods: {
-
+      playSound () {
+        sound.play()
+      }
     }
   }
 </script>
