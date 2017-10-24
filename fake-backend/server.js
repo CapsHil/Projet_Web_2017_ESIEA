0f1b3994bac3cea5a966841bcfef8833e3151a64
Pierre RABY <CapsHil@users.noreply.github.com>
2017-10-24 02:53:06 +0200

    Update server.js

--- fake-backend/server.js
+++ fake-backend/server.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = express();
 
 app.get('/api/extract', function(req, res) {
-	console.log("GET request to '/extract'")
+	console.log("GET request to '/api/extract'")
 	let answer = Math.floor(Math.random() * (4 - 1 + 1));
 	res.setHeader('Content-Type', 'application/json')
 	res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080')
