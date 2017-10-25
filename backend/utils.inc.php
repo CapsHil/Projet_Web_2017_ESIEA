3a4b3139692fa58c48b984805020798939307b45
Pierre RABY <CapsHil@users.noreply.github.com>
2017-10-25 15:31:16 +0200

    Update utils.inc.php

--- backend/utils.inc.php
+++ backend/utils.inc.php
@@ -10,7 +10,7 @@ function returnHeader()
 {
 	header('Access-Control-Allow-Origin: *');
 	header('Access-Control-Allow-Methods : GET,POST,PUT,DELETE,OPTIONS');
-	header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
+	header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Access-Control-Allow-Origin, Access-Control-Allow-Methods, Authorization, X-Requested-With');
 }
 
 function logError($log=null)
