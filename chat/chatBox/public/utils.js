cdff418c54575c82fb5ac4577763cfc81abe4bc3
Administrator <vgilles@et.esiea.fr>
2017-10-29 21:57:01 +0100

    [CLEAN] removing unused code

--- chat/chatBox/public/utils.js
+++
@@ -1,9 +0,0 @@
-/*jslint browser: true*/
-
-var utils = {
-    scrollToBottom: function() {
-        if ($(window).scrollTop() + $(window).height() + 2 * $('#messages li').last().outerHeight() >= $(document).height()) {
-            $('html, body').animate({ scrollTop: $(document).height() }, 0);
-        }
-    }
-};
